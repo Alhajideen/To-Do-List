894352b5fcdcc53383f3ca359a880272c12d4717
Alhajideen <salifunurudeen834@gmail.com>
2022-09-27 14:35:06 -0600

    add and export content in Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,29 @@
-const Home
+const Home = (icon) => {
+  return `<div class='to-do'>
+    <div class='header'>
+      <h1>Today's To Do</h1>
+      <span>
+        <i class='fa-solid fa-rotate'></i>
+      </span>
+    </div>
+    <div class='input-field'>
+      <input type='text' placeholder='Add to your list' />
+      <img class="arrow-left" src=${icon} alt='' />
+    </div>
+    <div class='todo-list'>
+      <div class='text-box'>
+        <input type='checkbox' />
+        <h2>Lorem ipsum dolor sit amet.</h2>
+      </div>
+      <div class='edit-icon'>
+        <i class='fa-regular fa-ellipsis-vertical'></i>
+      </div>
+      
+    </div>
+    <div class='clear-cmpl'>
+        <button>Clear all completed</button>
+      </div>
+  </div>`;
+};
+
+export default Home;
